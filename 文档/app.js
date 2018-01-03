3824e417d884fb88dd7849573baa5febe06dccb8
liumiao <1042887894@qq.com>
2018-01-03 20:08:52 +0800

    文章发布页面

---
+++ 文档/app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+app.use(express.static('lib/utf8-php'));
+
+//app.get('/',function(req,res){
+//	app.
+//})
+
+app.post("/addComm",function(req,res){
+	res.append("Access-Control-Allow-Origin","*");
+	res.send("ok");	
+})
+app.listen(9999);
+module.exports = app;
+
